ce875d78864d2efec4cda1d9dc6ee0c8a3ee4aef
Alex Soto <asoto@innventto.com>
2016-04-20 20:20:00 -0500

    Stage api added

--- es6/config/config.js
+++ es6/config/config.js
@@ -5,6 +5,10 @@ var config = {
     apiUrl: "http://localhost:4000",
     regex: /http:\/\/localhost:4000\/static\/.*?\/images\//g
   },
+  stage: {
+  	apiUrl: "http://www.nigma.api.grupo-guiame.org/",
+    regex: /http:\/\/www\.nigma\.api\.grupo-guiame\.org\/static\/.*?\/images\//g
+  }
   production: {
     apiUrl: "http://104.131.58.229:4000",
     regex: /http:\/\/104.131.58.229:4000\/static\/.*?\/images\//g
